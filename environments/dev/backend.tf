16d0e6383e6842786731505f249690689f5042d7
sriram123git <106502750+sriram123git@users.noreply.github.com>
2026-06-25 15:34:40 +0530

    Update S3 bucket name for Terraform backend

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "sriraman-terraform-state-2025"
+    bucket       = "sriraman-terraform-state-2026"
     key          = "dev/terraform.tfstate"
     region       = "us-east-1"
     use_lockfile = true
